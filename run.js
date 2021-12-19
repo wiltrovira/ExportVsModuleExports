df6b48c0b72e417c209f206062a821e93cad1478
Wilt Rovira <wrovira@gmail.com>
2021-12-19 12:58:22 -0500

    Mejores mensajes de salida para los ejemplos

--- run.js
+++ run.js
@@ -1,24 +1,25 @@
 //Imprime las propiedades module
 console.log("\nEJEMPLO 1");
-console.log(module);
+console.log("exports ==> ", exports);
+console.log("module ==> ", module);
 
 //exports is a reference to modules.exports. 
 console.log("\nEJEMPLO 2");
+console.log("Se asignan los valores de exports.a y exports.b")
 exports.a = "A";
 exports.b = "B";
-console.log(module);
+console.log("Son iguales? ==> ", exports === module.exports);
+console.log("exports ==> ", exports);
+console.log("module ==> ", module);
 
-// EJEMPLO 3
-//Assign properties to the exports object is a neat shortcut if you want to export an object from your module
-console.log("\nEJEMPLO 3");
-console.log(module);
-console.log(exports === module.exports);
-console.log(module.exports);
-
-//EJEMPLO 4
+//EJEMPLO 3
 // If you assign anything to module.exports, exports is not no longer a reference to it, and exports loses all its power.
-console.log("\nEJEMPLO 4");
+console.log("\nEJEMPLO 3");
+console.log("Se asigna la propiedad module.exports");
 module.exports = { a: "A" };
+console.log("Luego se asigna exports.b");
 exports.b = "B";  //esta propiedad se ignora
-console.log(exports === module.exports);
-console.log(module);
+console.log("Son iguales? ==> ", exports === module.exports);
+console.log("exports ==> ", exports);
+console.log("module ==> ", module);
+
